e7106c755d96da89441a2364bf9ed2ab0282f11f
Richard Adnams <rick@ironfang.uk>
2024-12-11 13:36:10 +0000

    tidy

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/ironfang-ltd/ironscript/evaluator"
 	"github.com/ironfang-ltd/ironscript/lexer"
@@ -15,7 +16,7 @@ func main() {
 <h2>Items ({% print(count(items)) %}): </h2>
 {% if (count(items) > 0) { %}
 <p>There are items</p>
-{% } %}
+{% } %} 
 <ul>
 	{% foreach (items as item) { %}
 	<li>{% print(item.name) %}</li>
@@ -32,7 +33,7 @@ func main() {
 		fmt.Println(err)
 	} else {
 
-		eval := evaluator.New()
+		eval := evaluator.New(os.Stdout)
 
 		eval.RegisterFunction("count", func(args ...evaluator.Object) (evaluator.Object, error) {
 			if len(args) != 1 {
@@ -71,57 +72,4 @@ func main() {
 			fmt.Println("Output:---\n", eval.GetOutput())
 		}
 	}
-
-	/*script := `
-	fn add(a, b) {
-		return a + b;
-	}
-
-	let result = add(5, 10);
-
-	print (result);
-	`
-
-		l := lexer.New(script)
-		p := parser.New(l)
-
-		program, err := p.Parse()
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		eval := evaluator.New()
-
-		eval.RegisterFunction("print", func(args ...evaluator.Object) (evaluator.Object, error) {
-			for _, arg := range args {
-				fmt.Println(arg.Debug())
-			}
-			return evaluator.Null, nil
-		})
-
-		eval.RegisterFunction("add", func(args ...evaluator.Object) (evaluator.Object, error) {
-			if len(args) != 2 {
-				return evaluator.Null, fmt.Errorf("wrong number of arguments. got=%d, want=2", len(args))
-			}
-
-			left, ok := args[0].(*evaluator.IntegerValue)
-			if !ok {
-				return evaluator.Null, fmt.Errorf("arguments to `add` must be INTEGER, got %s and %s", args[0].Type(), args[1].Type())
-			}
-
-			right, ok := args[1].(*evaluator.IntegerValue)
-			if !ok {
-				return evaluator.Null, fmt.Errorf("arguments to `add` must be INTEGER, got %s and %s", args[0].Type(), args[1].Type())
-			}
-
-			return &evaluator.IntegerValue{Value: left.Value + right.Value}, nil
-		})
-
-		scope := evaluator.NewScope()
-		result, err := eval.Evaluate(program, scope)
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(result.Debug())
-		}*/
 }
