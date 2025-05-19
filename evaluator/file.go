f8f9a671841becdd68ecf79a27ea695252a3c331
Richard Adnams <rick@ironfang.uk>
2025-05-19 16:50:10 +0100

    added fileName

--- evaluator/file.go
+++ evaluator/file.go
@@ -8,21 +8,23 @@ import (
 type FileValue struct {
 	FileID      string
 	FilePath    string
+	FileName    string
 	FileSize    int64
 	ContentType string
 }
 
-func NewFileValue(fileID, filePath, contentType string, fileSize int64) *FileValue {
+func NewFileValue(fileID, filePath, fileName, contentType string, fileSize int64) *FileValue {
 	return &FileValue{
 		FileID:      fileID,
 		FilePath:    filePath,
+		FileName:    fileName,
 		FileSize:    fileSize,
 		ContentType: contentType,
 	}
 }
 
 func (s *FileValue) Debug() string {
-	return fmt.Sprintf("<file: %s, size: %d, contentType: %s>", s.FilePath, s.FileSize, s.ContentType)
+	return fmt.Sprintf("<file: %s, size: %d, contentType: %s>", s.FileName, s.FileSize, s.ContentType)
 }
 
 func (s *FileValue) Type() ObjectType {
